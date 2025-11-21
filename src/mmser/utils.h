176f611ed968cefe846d004971b6a1e676d899c9
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2025-11-21 21:51:48 +0100

    feat: load/save files via mmap

--- src/mmser/utils.h
+++ src/mmser/utils.h
@@ -5,6 +5,20 @@
 #include "Archive.h"
 #include "Handler.h"
 
+#include <any>
+#include <fstream>
+#include <iostream>
+
+#if (defined(unix) || defined(__unix__) || defined(__unix))
+    #include <fcntl.h>
+    #include <sys/mman.h>
+    #include <sys/stat.h>
+    #include <tuple>
+    #include <unistd.h>
+    #define MMSER_MMAP
+#endif
+
+
 namespace mmser {
 inline auto requiredPaddingBytes(size_t totalSize, size_t alignment) -> size_t {
     size_t usedBytesOfNextElement = totalSize % alignment;
@@ -36,5 +50,110 @@ size_t computeSaveSize(T const& t) {
     auto archive = Archive<Mode::SaveSize>{};
     handle(archive, t);
     return archive.totalSize;
+}
+
+using Storage = std::unique_ptr<std::any>;
+
+template <typename T>
+auto loadFileCopy(std::filesystem::path const& path) -> std::tuple<T, Storage> {
+    auto ret = std::tuple<T, Storage>{};
+
+    auto buffer = std::vector<char>{};
+    {
+        auto file = std::ifstream{path, std::ios::in | std::ios::binary | std::ios::ate};
+        buffer.resize(file.tellg());
+        file.seekg(0, std::ios::beg);
+        file.read(buffer.data(), buffer.size());
+    }
+    load(buffer, std::get<0>(ret));
+    return ret;
+}
+
+#ifdef MMSER_MMAP
+template <typename T>
+auto loadFileMMap(std::filesystem::path const& path) -> std::tuple<T, Storage> {
+    auto ret = std::tuple<T, Storage>{};
+
+    auto file_fd = ::open(path.c_str(), O_RDONLY);
+    if (file_fd == -1) {
+        throw std::runtime_error{"file " + path.string() + " not readable"};
+    }
+    auto file_size = static_cast<size_t>(std::filesystem::file_size(path));
+    auto ptr = (char const*)mmap(nullptr, file_size, PROT_READ, MAP_PRIVATE, file_fd, 0);
+    if (ptr == MAP_FAILED) {
+        throw std::runtime_error{"mmap failed"};
+    }
+    auto buffer = std::span<char const>{ptr, file_size};
+    load(buffer, std::get<0>(ret));
+    return ret;
+}
+#endif
+
+
+template <typename T>
+auto loadFile(std::filesystem::path const& path) -> std::tuple<T, Storage> {
+    #ifdef MMSER_MMAP
+        return loadFileMMap<T>(path);
+    #else
+        return loadFileCopy<T>(path);
+    #endif
+}
+
+template <typename T>
+void saveFileCopy(std::filesystem::path const& path, T const& t) {
+    auto size = computeSaveSize(t);
+
+    auto buffer = std::vector<char>{};
+    buffer.resize(size);
+    save(buffer, t);
+    {
+        auto file = std::ofstream{path, std::ios::out | std::ios::binary | std::ios::trunc};
+        file.write(buffer.data(), buffer.size());
+    }
+}
+
+#ifdef MMSER_MMAP
+template <typename T>
+void saveFileMMap(std::filesystem::path const& path, T const& t) {
+    auto size = computeSaveSize(t);
+
+    auto file_fd = ::open(path.c_str(), O_RDWR | O_CREAT | O_TRUNC, 0666);
+    if (file_fd == -1) {
+        throw std::runtime_error{"file " + path.string() + " not writable"};
+    }
+
+    if (size > 0) {
+        if (auto r = lseek(file_fd, size-1, SEEK_SET); r == -1) {
+            close(file_fd);
+            throw std::runtime_error{"file " + path.string() + " not writable, ::lseek error"};
+        }
+        if (auto r = write(file_fd, "", 1); r != 1) {
+            close(file_fd);
+            throw std::runtime_error{"file " + path.string() + " not writable, ::write error"};
+        }
+        auto ptr = (char*)mmap(nullptr, size, PROT_WRITE, MAP_SHARED, file_fd, 0);
+        if (ptr == MAP_FAILED) {
+            close(file_fd);
+            throw std::runtime_error{"mmap failed"};
+        }
+        auto buffer = std::span<char>{ptr, size};
+        save(buffer, t);
+        if (auto r = munmap((void*)ptr, size); r != 0) {
+            throw std::runtime_error{std::string{"munmap failed: "} + strerror(errno) + "(" + std::to_string(errno) + ")"};
+        }
+    }
+    if (auto r = close(file_fd); r != 0) {
+        throw std::runtime_error{"::close failed"};
+    }
+}
+#endif
+
+template <typename T>
+void saveFile(std::filesystem::path const& path, T const& t) {
+    #ifdef MMSER_MMAP
+        saveFileMMap(path, t);
+    #else
+        saveFileCopy(path, t);
+    #endif
 }
 }
